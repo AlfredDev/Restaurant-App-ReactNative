7f466cc8ab0b1bc42429dffa11eb40e7e1eda001
Alfred <alfredgarcia14200@gmail.com>
2022-11-22 23:29:54 -0600

    fecha corregida

--- src/screens/TabScrens/Account.jsx
+++ src/screens/TabScrens/Account.jsx
@@ -1,29 +1,37 @@
-import React, { useContext, useEffect,useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text } from "react-native";
 import { View } from "react-native-animatable";
 import { Header } from "../../components/Header";
 import { Button, Stack } from "@react-native-material/core";
-import { collection, deleteDoc, getDocs, query, where, doc,orderBy } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  getDocs,
+  query,
+  where,
+  doc,
+  orderBy,
+} from "firebase/firestore";
 import { db } from "../../../database/firebase";
 import { OrdenesListas } from "../../components/OrdenesListas";
 import { Checkbox } from "react-native-paper";
 
 import { UserContext } from "../../hooks/UserContext";
 
-export const Account = ({navigation}) => {
- // const [checked, setChecked] = React.useState(false);
+export const Account = ({ navigation }) => {
+  // const [checked, setChecked] = React.useState(false);
   const [orden, setOrdenes] = useState([]);
   const { ordenes } = useContext(UserContext);
 
   async function fetchData() {
-    const q = query(collection(db, "Orden"),orderBy("fk_mesa_id"));
+    const q = query(collection(db, "Orden"), orderBy("fk_mesa_id"));
     const querySnapshot = await getDocs(q);
     const ordenes = [];
     querySnapshot.forEach((doc) => {
-      const { fk_mesa_id,folio,estatus,fk_cuenta_id} = doc.data();
+      const { fk_mesa_id, folio, estatus, fk_cuenta_id } = doc.data();
       ordenes.push({
         //id: id,
-        idDoc:doc.id,
+        idDoc: doc.id,
         estatus: estatus,
         folio: folio,
         fk_mesa_id: fk_mesa_id,
@@ -42,25 +50,22 @@ export const Account = ({navigation}) => {
     <View style={styles.container}>
       <Header titulo={"Órdenes"} />
       <ScrollView stickyHeaderIndices={[1]}>
-      <View style={styles.container}>
-      <Stack spacing={10} style={[{ margin: 16 }, { marginTop: 10 }]}>
-                {orden.map((op) => (
-                  <OrdenesListas
-                    key= {op.idDoc}
-                    folio={op.folio}
-                    fk_mesa_id={op.fk_mesa_id}
-                    fk_cuenta_id={op.fk_cuenta_id}
-                    estatus={op.estatus}
-                    navigation = {navigation}
-                    op={op}
-                    //estatus={op.estatus}
-                    
-                  />
-                 
-                ))}
-                
-              </Stack>
-              </View>
+        <View style={styles.container}>
+          <Stack spacing={10} style={[{ margin: 16 }, { marginTop: 10 }]}>
+            {orden.map((op) => (
+              <OrdenesListas
+                key={op.idDoc}
+                folio={op.folio}
+                fk_mesa_id={op.fk_mesa_id}
+                fk_cuenta_id={op.fk_cuenta_id}
+                estatus={op.estatus}
+                navigation={navigation}
+                op={op}
+                //estatus={op.estatus}
+              />
+            ))}
+          </Stack>
+        </View>
       </ScrollView>
     </View>
   );
