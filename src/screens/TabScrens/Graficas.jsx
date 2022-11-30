c7fff20858726eb0b75331da7711dea72518748f
Alfred <alfredgarcia14200@gmail.com>
2022-11-29 23:18:27 -0600

    Ventana de graficas

--- src/screens/TabScrens/Graficas.jsx
+++ src/screens/TabScrens/Graficas.jsx
@@ -1,11 +1,67 @@
 import { Button, Stack } from "@react-native-material/core";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import {
+  RefreshControl,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { db } from "../../../database/firebase";
 import { HeaderOnly } from "../../components/HeaderOnly";
 import { theme } from "../../core/theme";
 import { getFecha } from "../../helpers/Backed";
 
 export const Graficas = () => {
+  const [venta, setVenta] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    fetchData();
+    setRefreshing(false);
+  });
+
+  const fecthOrdenes = async () => {
+    const objRef = collection(db, "Venta");
+    const q = query(objRef, where("fecha", "==", getFecha()));
+    const querySnapshot = await getDocs(q);
+
+    const cuentas = [];
+    // const pedido = [];
+
+    querySnapshot.forEach((doc) => {
+      const { cliente, fecha, id, mesa, total } = doc.data();
+
+      let cuenta = {
+        cliente: cliente,
+        fecha: fecha,
+        id: id,
+        mesa: mesa,
+        total: total,
+      };
+
+      // pedido.push(pedidos);
+      cuentas.push(cuenta);
+    });
+    setVenta(cuentas);
+
+    let t = 0;
+
+    venta.forEach((obj) => {
+      t += obj.total;
+    });
+
+    setTotal(t);
+  };
+
+  useEffect(() => {
+    fecthOrdenes();
+    console.log(venta);
+  }, []);
+
   return (
     <View style={styles.container}>
       <HeaderOnly descripcion={"Reporte ventas"} />
@@ -16,6 +72,12 @@ export const Graficas = () => {
           </Text>
           <Text style={{ fontSize: 16, fontWeight: "450" }}>{getFecha()}</Text>
         </View>
+        <View style={styles.total}>
+          <Text>$ {total}</Text>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.link}>Ver detalles</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.botones}>
         <Stack spacing={2} style={{ margin: 16 }}>
@@ -88,5 +150,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 10,
+  },
+  total: {
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "#D8D2CB",
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  link: {
+    textAlign: "right",
+    marginRight: 20,
+    color: theme.colors.primary,
   },
 });
